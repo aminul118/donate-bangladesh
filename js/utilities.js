ede1ea4278b61e8705389be1af3e89959fdabd99
Md Aminul Islam <mr.aminul118@gmail.com>
2024-09-22 20:31:58 +0600

    Landing Page design

--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,16 @@
 // console.log('I am utility');
+
+// ! Function for getting Input Value
+function getInputValueById(id) {
+  const inputValueText = document.getElementById(id).value;
+  const inputNumber = parseFloat(inputValueText);
+  return inputNumber;
+}
+
+function getTextValueById(id) {
+  const textValue = document.getElementById(id).innerText;
+  const textNumber = parseFloat(textValue);
+  return textNumber;
+}
+
+
