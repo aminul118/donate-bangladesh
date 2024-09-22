250cb7cc16326980fede65211648a57566d32d1b
Md Aminul Islam <mr.aminul118@gmail.com>
2024-09-23 02:15:11 +0600

    Feni function add and Validation added

--- js/donate.js
+++ js/donate.js
@@ -1,6 +1,4 @@
 // Donate for Flood at Noakhali, Bangladesh
-// Donate Part
-
 document.getElementById('noakhali-donate-btn')
     .addEventListener('click', function (event) {
         event.preventDefault();
@@ -10,6 +8,10 @@ document.getElementById('noakhali-donate-btn')
         const noakhaliGotDonation = getTextValueById('noakhali-donation-amount');
         const noakhaliTotalDonation = noakhaliGotDonation + donationAmount;
 
+        if (mainDonationBalance < donationAmount) {
+            alert('You have not enoung Balance');
+            return;
+        }
 
         if (donationAmount > 0 && isNaN(donationAmount) == !true) {
             document.getElementById('noakhali-donation-amount').innerText = noakhaliTotalDonation;
@@ -20,4 +22,29 @@ document.getElementById('noakhali-donate-btn')
             alert('Invalid Input')
         }
 
+    })
+
+// Donate for Flood Relief in Feni,Bangladesh
+document.getElementById('feni-donate-btn')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        const mainDonationBalance = getTextValueById('main-donation-balance');
+        const donationAmount = getInputValueById('feni-input-field');
+        const balanceRemaining = mainDonationBalance - donationAmount;  //Going to top right bar
+        const feniGotDonation = getTextValueById('feni-donation-amount');
+        const feniTotalDonation = feniGotDonation + donationAmount;
+
+        if (mainDonationBalance < donationAmount) {
+            alert('You have not enoung Balance');
+            return;
+        }
+
+        if (donationAmount > 0 && isNaN(donationAmount) == !true) {
+            document.getElementById('feni-donation-amount').innerText = feniTotalDonation;
+            document.getElementById('main-donation-balance').innerText = balanceRemaining;
+
+        }
+        else {
+            alert('Invalid Input')
+        }
     })
