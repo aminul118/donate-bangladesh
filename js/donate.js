c781aedb2b320f3071cda951fc3b260abf072168
Md Aminul Islam <mr.aminul118@gmail.com>
2024-09-23 19:34:50 +0600

    Modal Message Complect

--- js/donate.js
+++ js/donate.js
@@ -1,4 +1,4 @@
-// Donate for Flood at Noakhali, Bangladesh
+//! Donate for Flood at Noakhali, Bangladesh
 document.getElementById('noakhali-donate-btn')
     .addEventListener('click', function (event) {
         event.preventDefault();
@@ -9,13 +9,19 @@ document.getElementById('noakhali-donate-btn')
         const noakhaliTotalDonation = noakhaliGotDonation + donationAmount;
 
         if (mainDonationBalance < donationAmount) {
-            alert('You have not enoung Balance');
+            alert('You have not enough Balance');
             return;
         }
 
         if (donationAmount > 0 && isNaN(donationAmount) == !true) {
             document.getElementById('noakhali-donation-amount').innerText = noakhaliTotalDonation;
             document.getElementById('main-donation-balance').innerText = balanceRemaining;
+            document.getElementById('my_modal_5').showModal();
+
+
+            const p = document.createElement('p');
+            p.innerText = `You Donate ${donationAmount} BDT for Flood at Noakhali, Bangladesh`
+            document.getElementById('modal-container').appendChild(p);
 
         }
         else {
@@ -24,7 +30,7 @@ document.getElementById('noakhali-donate-btn')
 
     })
 
-// Donate for Flood Relief in Feni,Bangladesh
+//! Donate for Flood Relief in Feni,Bangladesh
 document.getElementById('feni-donate-btn')
     .addEventListener('click', function (event) {
         event.preventDefault();
@@ -35,18 +41,66 @@ document.getElementById('feni-donate-btn')
         const feniTotalDonation = feniGotDonation + donationAmount;
 
         if (mainDonationBalance < donationAmount) {
-            alert('You have not enoung Balance');
+            alert('You have not enough Balance');
             return;
         }
 
         if (donationAmount > 0 && isNaN(donationAmount) == !true) {
             document.getElementById('feni-donation-amount').innerText = feniTotalDonation;
             document.getElementById('main-donation-balance').innerText = balanceRemaining;
-          
+            document.getElementById('my_modal_5').showModal();
+
+            const p = document.createElement('p');
+            p.innerText = `You Donate ${donationAmount} BDT for Flood Relief in Feni,Bangladesh`
+            document.getElementById('modal-container').appendChild(p);
+
 
         }
         else {
             alert('Invalid Input')
-           
+
         }
     })
+
+//! Donate for Injured in the Quota Movement
+
+document.getElementById('quota-input-btn')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        const mainDonationBalance = getTextValueById('main-donation-balance');
+        const donationAmount = getInputValueById('quota-input-field');
+        const balanceRemaining = mainDonationBalance - donationAmount;  //Going to top right bar
+        const quotaGotDonation = getTextValueById('quota-donation-amount');
+        const quotaTotalDonation = quotaGotDonation + donationAmount;
+
+        if (mainDonationBalance < donationAmount) {
+            alert('You have not enough Balance');
+            return;
+        }
+
+        if (donationAmount > 0 && isNaN(donationAmount) == !true) {
+            document.getElementById('quota-donation-amount').innerText = quotaTotalDonation;
+            document.getElementById('main-donation-balance').innerText = balanceRemaining;
+            document.getElementById('my_modal_5').showModal();
+
+            const p = document.createElement('p');
+            p.innerText = `You Donate ${donationAmount} BDT for Injured in the Quota Movement`
+            document.getElementById('modal-container').appendChild(p);
+
+        }
+        else {
+            alert('Invalid Input')
+
+        }
+    })
+    
+    // Modal Message Reset
+document.getElementById('close-modal-btn').addEventListener('click', function () {
+    const modal = document.getElementById('my_modal_5');
+    modal.close();
+
+    setTimeout(function () {
+        const modalMessage = document.getElementById('modal-container');
+        modalMessage.innerText = '';  
+    }, ); 
+});
